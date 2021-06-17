ea8e92a60fa5e33ca31a3295a5bf7f1e59bef1dd
Supermob1 <48822797+Supermob1@users.noreply.github.com>
2021-06-17 21:58:29 +0200

    lol

--- Development/hoi4 flag maker/flagmaker.bat
+++ Development/hoi4 flag maker/flagmaker.bat
@@ -3,24 +3,15 @@ set /p tag="Enter tag:"
 
 if not exist flags mkdir flags
 magick convert -verbose flag.tga -resize 82x52! flag1.tga
-copy flag1.tga flags\%tag%_equality.tga
-copy flag1.tga flags\%tag%_order.tga
-copy flag1.tga flags\%tag%_liberty.tga
-copy flag1.tga flags\%tag%_glory.tga
+copy flag1.tga flags\%tag%.tga
 del flag1.tga
 
 if not exist flags\medium mkdir flags\medium
 magick convert flag.tga -resize 41x26! flag1.tga
-copy flag1.tga flags\medium\%tag%_equality.tga
-copy flag1.tga flags\medium\%tag%_order.tga
-copy flag1.tga flags\medium\%tag%_liberty.tga
-copy flag1.tga flags\medium\%tag%_glory.tga
+copy flag1.tga flags\medium\%tag%.tga
 del flag1.tga
 
 if not exist flags\small mkdir flags\small
 magick convert flag.tga -resize 10x7! flag1.tga
-copy flag1.tga flags\small\%tag%_equality.tga
-copy flag1.tga flags\small\%tag%_order.tga
-copy flag1.tga flags\small\%tag%_liberty.tga
-copy flag1.tga flags\small\%tag%_glory.tga
+copy flag1.tga flags\small\%tag%.tga
 del flag1.tga
